9cf16fe028dba6a85cae208442cbd912f5fee877
marvin <marvinrenelopez@gmail.com>
2021-02-22 16:03:13 -0600

    question functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,24 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  int questionNumber = 0;
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: false),
+    Question(q: 'A slug\'s blood is green.', a: false),
+  ];
+
+  List<bool> answers = [
+    false,
+    false,
+    true,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +80,24 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer;
+
+                if (true == correctAnswer) {
+                  print('correct');
+                } else {
+                  print('wrong');
+                }
+
+                setState(() {
+                  questionNumber++; // = questionNumber + 1;
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                });
               },
             ),
           ),
@@ -79,19 +115,36 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  questionNumber = questionNumber + 1;
+
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+
+List<String> questions = [
+'You can lead a cow down stairs but not up stairs.', false,
+'Approximately one quarter of human bones are in the feet.', true,
+'A slug\'s blood is green.', true,
+]
+
+
+
 */
